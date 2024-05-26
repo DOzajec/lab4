1bb355ffb669512897c739712cf4c44cc6de9e2d
DOzajec <dsw51705@student.dsw.edu.pl>
2024-05-26 11:08:30 +0200

    ile logs

--- skrypt.sh
+++ skrypt.sh
@@ -6,7 +6,11 @@ case $1 in
 	date
 	;;
 	--logs)
-	for i in {1..100}; do
+	ilosc=100
+	if [ ! -z "$2" ]; then
+		ilosc=$2
+	fi
+	for i in $(seq 1 $ilosc); do
 		plik="log${i}.txt"
 		echo -e "Nazwa pliku: $plik \nNazwa skryptu: skrypt.sh \nData: $(date)" > $plik
 	done
