07da5b76cedee75c85987b7f766467c0a873632a
DOzajec <dsw51705@student.dsw.edu.pl>
2024-05-26 11:37:49 +0200

    error

--- skrypt.sh
+++ skrypt.sh
@@ -19,6 +19,17 @@ case $1 in
 	git clone https://github.com/DOzajec/lab4.git
 	export PATH=$PATH:$(pwd)/lab4
 	;;
+	--error|-e)
+	num_files=100
+    	if [ ! -z "$2" ]; then
+      		ilosc=$2
+    	fi
+    	for i in $(seq 1 $ilosc); do
+      		mkdir -p "error${i}"
+      		plik="error${i}/error${i}.txt"
+      		echo -e "Nazwa pliku: $plik" > $plik
+    	done
+	;;
 	--help)
 	echo "--date	Wyświetlenie dzisiejszej daty"
 	echo "--logs	Generuje automatycznie 100 plików log z nazwą pliku, skryptu który to wykonał i dzisiejszą datą"
